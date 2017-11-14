33645c283d9877dfe153a6707d81c0ce0b1d7a2b
super456 <1061996681@qq.com>
2017-11-14 16:12:56 +0800

    添加存储学生评教课程的评教卡内容信息包

---
+++ src/student/bean/CommentContents.java
@@ -0,0 +1,85 @@
+package student.bean;
+//用于存储学生评教课程的评教卡内容信息
+
+public class CommentContents {
+
+	int comConNum;//获取评教卡编号
+	String theFirstCon=null;//获取评教卡第一项内容
+	String theSecondCon=null;//获取评教卡第二项内容
+	String theThirdCon=null;//
+	String theFourthCon=null;//
+	String theFifthCon=null;//
+	String theSixthCon=null;
+	String theSeventhCon=null;
+	String theEighthCon=null;
+	String theNinthCon=null;
+	String theTenthCon=null;
+	public int getComConNum() {
+		return comConNum;
+	}
+	public void setComConNum(int comConNum) {
+		this.comConNum = comConNum;
+	}
+	public String getTheFirstCon() {
+		return theFirstCon;
+	}
+	public void setTheFirstCon(String theFirstCon) {
+		this.theFirstCon = theFirstCon;
+	}
+	public String getTheSecondCon() {
+		return theSecondCon;
+	}
+	public void setTheSecondCon(String theSecondCon) {
+		this.theSecondCon = theSecondCon;
+	}
+	public String getTheThirdCon() {
+		return theThirdCon;
+	}
+	public void setTheThirdCon(String theThirdCon) {
+		this.theThirdCon = theThirdCon;
+	}
+	public String getTheFourthCon() {
+		return theFourthCon;
+	}
+	public void setTheFourthCon(String theFourthCon) {
+		this.theFourthCon = theFourthCon;
+	}
+	public String getTheFifthCon() {
+		return theFifthCon;
+	}
+	public void setTheFifthCon(String theFifthCon) {
+		this.theFifthCon = theFifthCon;
+	}
+	public String getTheSixthCon() {
+		return theSixthCon;
+	}
+	public void setTheSixthCon(String theSixthCon) {
+		this.theSixthCon = theSixthCon;
+	}
+	public String getTheSeventhCon() {
+		return theSeventhCon;
+	}
+	public void setTheSeventhCon(String theSeventhCon) {
+		this.theSeventhCon = theSeventhCon;
+	}
+	public String getTheEighthCon() {
+		return theEighthCon;
+	}
+	public void setTheEighthCon(String theEighthCon) {
+		this.theEighthCon = theEighthCon;
+	}
+	public String getTheNinthCon() {
+		return theNinthCon;
+	}
+	public void setTheNinthCon(String theNinthCon) {
+		this.theNinthCon = theNinthCon;
+	}
+	public String getTheTenthCon() {
+		return theTenthCon;
+	}
+	public void setTheTenthCon(String theTenthCon) {
+		this.theTenthCon = theTenthCon;
+	}
+	
+	
+}
