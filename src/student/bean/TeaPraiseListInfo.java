06ed0a4c9b9fae994a97cabb48083d8ae7a1b4a2
super456 <1061996681@qq.com>
2017-11-16 00:12:05 +0800

    添加存储教师好评榜信息类

---
+++ src/student/bean/TeaPraiseListInfo.java
@@ -0,0 +1,58 @@
+package student.bean;
+//存储教师好评榜的信息
+public class TeaPraiseListInfo {
+
+	String couName=null;
+	String teaName=null;
+	String couFrom=null;
+	String couTime=null;
+	String couPlace=null;
+	int couCredit;
+	float theAllAvgScore;
+	public String getCouName() {
+		return couName;
+	}
+	public void setCouName(String couName) {
+		this.couName = couName;
+	}
+	public String getTeaName() {
+		return teaName;
+	}
+	public void setTeaName(String teaName) {
+		this.teaName = teaName;
+	}
+	public String getCouFrom() {
+		return couFrom;
+	}
+	public void setCouFrom(String couFrom) {
+		this.couFrom = couFrom;
+	}
+	public String getCouTime() {
+		return couTime;
+	}
+	public void setCouTime(String couTime) {
+		this.couTime = couTime;
+	}
+	public String getCouPlace() {
+		return couPlace;
+	}
+	public void setCouPlace(String couPlace) {
+		this.couPlace = couPlace;
+	}
+	public int getCouCredit() {
+		return couCredit;
+	}
+	public void setCouCredit(int couCredit) {
+		this.couCredit = couCredit;
+	}
+	public float getTheAllAvgScore() {
+		return theAllAvgScore;
+	}
+	public void setTheAllAvgScore(float theAllAvgScore) {
+		this.theAllAvgScore = theAllAvgScore;
+	}
+	
+	
+	
+	
+}
